4a7ee1652c9b4bf534c049ea69b7c8ee3398936e
Mark Sommers <mssommer@ncsu.edu>
2021-01-21 15:49:45 -0500

    added heroku deployments

--- src/app.ts
+++ src/app.ts
@@ -29,14 +29,14 @@ export class App {
         // Listen on port 8000 for calls
         this.app.listen(process.env.PORT || 8000, () => {
             // tslint:disable-next-line:no-console
-            console.log("Grade Tracker API listening on port 8000!");
+            console.log(`Grade Tracker API listening on port ${process.env.PORT || 8000}!`);
         });
     }
 
     private config() {
         // Connect to mongoose database
         // tslint:disable-next-line:max-line-length
-        connect(process.env.MONGODB_URI || "mongodb://heroku_k1lm0tlz:vmn8kc43irbuvvckg2jtnk0vm2@ds131905.mlab.com:31905/heroku_k1lm0tlz", { useNewUrlParser: true });
+        connect(process.env.MONGODB_URI || "mongodb://host.docker.internal:27017", { useNewUrlParser: true });
 
         // Configure environment variables
         dotenv.config();
